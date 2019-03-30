f353e84629e220bff2214dbf29a35fc0de712076
Chudnovets Ivan <Mr.Chudvan@yandex.ru>
2019-03-30 13:11:17 +0300

    modified:   matrix.h 	modified:   test.cpp

--- 03/matrix.h
+++ 03/matrix.h
@@ -1,53 +1,23 @@
 #pragma once
 #include <iostream>
-class Proxy {
-	int *n;
-public:
-	void operator=(int a) {
-		*n = a;
-	}
-	void operator*=(int a) {
-		*n *= a;
-	}
-	bool operator !=(Proxy b) {
-		return (*n != b.getter());
-	}
-	Proxy(int &element) : n(&element) {};
-	int & getter() {
-		return *n;
-	}
-	~Proxy() {}
-};
+using namespace std;
 
 class Array {
-	int * list;
+	int *list;
+	int i;
 	int columns;
-	int errorInt = 1;
 public:
-	Proxy operator[](int j) {
-		try {
-			if (j < 1 || j > columns) throw std::out_of_range("");
-			return Proxy(list[j - 1]);
-		}
-		catch (std::out_of_range) {
-			std::cout << "Выход за пределы матрицы." << std::endl;
-			return Proxy(errorInt);
-		}
+	const int& operator[](int j) const{
+		if (j > columns) throw out_of_range("");
+		return list[i * columns + j - 1];
 	}
-	void declaration(int cols) {
-		list = new int[cols]();
-		columns = cols;
+	int& operator[](int j) {
+		if (j > columns) throw out_of_range("");
+		return list[i * columns + j - 1];
 	}
-	void setter(int j, int a) {
-		list[j] = a;
+	Array(int* m, size_t i, size_t columns) : list(m), i(i), columns(columns){
 	}
-	int getter(int j) {
-		return list[j];
-	}
-	Array() {}
-	Array(int error) {}
 	~Array() {
-		delete[] list;
 	}
 };
 
@@ -55,73 +25,64 @@ class Matrix
 {
 	size_t rows;
 	size_t columns;
-	Array errorArray;
-	Array *m;
+	int *m;
 public:
+	Matrix() {}
 	Matrix(size_t row, size_t column) {
 		rows = row;
 		columns = column;
-		m = new Array[rows];
-		for (int i = 0; i < rows; i++) {
-			m[i].declaration(columns);
-		}
+		m = new int[rows * columns]();
 	}
 	~Matrix() {
 		delete[] m;
 	}
-	size_t getRows() {
+	size_t getRows() const{
 		return rows;
 	}
-	size_t getColumns() {
+	size_t getColumns() const{
 		return columns;
 	}
-	void show() const
+	void show()
 	{
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				printf("%3d", m[i].getter(j));
+		for (size_t i = 0; i < rows; i++) {
+			for (size_t j = 0; j < columns; j++) {
+				printf("%3d", m[i * columns + j]);
 			}
-			std::cout << std::endl;
+			cout << endl;
 		}
-		std::cout << std::endl;
+		cout << endl;
 	}
-	void operator *=(int a) {
-		for (int i = 0; i < rows; i++) {
-			for (int j = 1; j <= columns; j++) {
-				m[i][j] *= a;
+	Matrix& operator *=(int a) {
+		for (size_t i = 0; i < rows; i++) {
+			for (size_t j = 0; j < columns; j++) {
+				m[i * columns + j] *= a;
 			}
 		}
+		return *this;
 	}
-	bool operator==(const Matrix &B) 
+	bool operator==(const Matrix& B)  const
 	{
 		if (this == &B)return true;
 		else if (!(rows == B.rows && columns == B.columns))return false;
 		else {
-			for (int i = 0; i < rows; i++) {
-				for (int j = 1; j <= columns; j++) {
-					if (m[i][j] != B.m[i][j]) return false;
+			for (size_t i = 0; i < rows; i++) {
+				for (size_t j = 0; j < columns; j++) {
+					if (m[i * columns + j] != B.m[i * columns + j]) return false;
 				}
 			}
 		}
 		return true;
 	}
-	bool operator !=(Matrix &B)
+	bool operator !=(const Matrix& B) const
 	{
 		return (!(*this == B));
 	}
-	Array& operator[](size_t i) {
-		try {
-			if (i < 1 || i > rows) throw std::out_of_range("");
-			return m[i - 1];
-		}
-		catch (std::out_of_range) {
-			std::cout << "Выход за пределы матрицы." << std::endl;
-			return errorArray;
-		}
+	const Array operator[](size_t i) const{
+		if (i > rows) throw out_of_range("");
+		return Array(m, i - 1, columns);
+	}
+	Array operator[](size_t i) {
+		if (i > rows) throw out_of_range("");
+		return Array(m, i - 1, columns);
 	}
 };
-
-std::ostream& operator<<(std::ostream& out, Proxy a) {
-	out << a.getter();
-	return out;
-}
--- 03/test.cpp
+++ 03/test.cpp
@@ -1,8 +1,22 @@
 #include "matrix.h"
+#include <iostream>
+using namespace std;
 int main() {
-	Matrix A(3, 4);
+	setlocale(LC_ALL, "");
+	const size_t rows = 5;
+	const size_t cols = 3;
+	Matrix A(rows, cols);
+	A.show();
 	A[2][1] = 5;
-	std::cout << A[2][1];
+	A.show();
+	A[1][1] = 7;
+	A *= 10;
+	A.show();
+	Matrix B(3, 4);
+	cout << (A.getColumns() == 3) << endl << (A.getRows() == 5) << endl;
+	cout << endl;
+	auto m2 = A *= 5;
+	m2.show();
 	int n;
 	std::cin >> n;
 	return 0;
