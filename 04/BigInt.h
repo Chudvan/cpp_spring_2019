b3c2c333f13b4a61581f5dd88adecb2d0ba777bc
Chudnovets Ivan <Mr.Chudvan@yandex.ru>
2019-03-31 16:11:22 +0300

    modified:   04/BigInt.h

--- 04/BigInt.h
+++ 04/BigInt.h
@@ -7,6 +7,32 @@
 #include <cmath>
 using namespace std;
 
+
+/*В задаче используется класс BigInt для работы с большими целыми числами.
+
+  Идея: хранить число в массиве int *bI, разбив его по 4 разряда в каждой ячейке массива int* bI
+  bI[0] = Число ячеек в числе (BigInt) * знак числа (-/+)
+  bI[abs(bI[0])] - последняя ячейка числа(BigInt). Число записано в обратном направлении:
+  Например: числу -392736452677368 будет соответствовать bI[101]=[-4][7368][5267][7364][392][0]...[0]
+
+  Идентификаторы:
+  BASELEN - число десятичных разрядов в одной ячейке массива int* bI
+  BASE - основание системы счисления
+  MAXLEN - максимальная длина числа BigInt в ячейках
+
+  Методы класса BigInt:
+  int64_t bigIntCMP(int64_t* la, int64_t*  lb) const; - СРАВНЕНИЕ КОЛ-ВА РАЗРЯДОВ у двух длинных чисел
+  int64_t max(int64_t a, int64_t b) const; - max двух чисел (int64_t)
+  int64_t* getBigInt() const; - получить указатель на *bI
+  void strTolong(string a, int64_t* la); - (string/char *) -> (int64_t *)(bI)
+  void intToStr(int64_t a, char* b); - (int64_t) -> (char *)
+  int64_t sgn(int64_t a) const; - sgn (-1/0/1)
+  int digitCount(int64_t a) const; - получить КОЛ-ВО РАЗРЯДОВ в числе (в 10-ричной системе)
+  int64_t atois(string str); - ПРЕОБРАЗОВАНИЕ (string)(НЕ const char *) -> (int64_t)
+
+  Все операторы(+, -, <, <=, ==, !=, =>, >) в двух вверсиях: для BigInt и для int64_t*/
+
+
 class BigInt {
 public:
 	BigInt() {
@@ -59,6 +85,8 @@ public:
 		return c;
 	}
 	BigInt operator-(const BigInt& value) const {
+		//УДОБНО, когда первый параметр принимает число, у которого
+		//не меньше разрядов (оно не меньше по модулю)
 		int64_t *bI_2, *bI_3;
 		bI_2 = value.getBigInt();
 		if (bI[0] * bI_2[0] < 0)return *this + (-value);//аналогично, если числа разного знака
@@ -66,7 +94,7 @@ public:
 		bI_3 = c.getBigInt();
 		int64_t m = abs(bI[0]);
 		int64_t buffer = 0;
-		if (bigIntCMP(bI, bI_2) == -1) { //число 1-е < 2-го НУЖНО СДЕЛАТЬ РАЗНОСТЬ ДЛЯ ЭТОГО СЛУЧАЯ ТОЖЕ
+		if (bigIntCMP(bI, bI_2) == -1) {//НЕУДОБНО, меняем параметры в УДОБНОМ порядке
 			return (-value) - (-*this);
 		}
 		for (int64_t i = 1; i <= m; i++) {
@@ -94,7 +122,6 @@ public:
 	}
 	bool operator<(const BigInt& value) const {
 		int64_t *bI_buffer_2 = value.getBigInt();
-		if (!abs(bI[0]) || !abs(bI_buffer_2[0])) throw "Ошибка вывода. Число не инициализировано.";
 		BigInt c = *this - value;
 		int64_t *bI_buffer = c.getBigInt();
 		return bI_buffer[0] < 0;
@@ -105,7 +132,6 @@ public:
 	}
 	bool operator>(const BigInt& value) const {
 		int64_t *bI_buffer_2 = value.getBigInt();
-		if (!abs(bI[0]) || !abs(bI_buffer_2[0])) throw "Ошибка вывода. Число не инициализировано.";
 		BigInt c = *this - value;
 		int64_t *bI_buffer = c.getBigInt();
 		return bI_buffer[0] > 0 && bI_buffer[1];
@@ -116,7 +142,6 @@ public:
 	}
 	bool operator==(const BigInt& value)const {
 		int64_t *bI_buffer_2 = value.getBigInt();
-		if (!abs(bI[0]) || !abs(bI_buffer_2[0])) throw "Ошибка вывода. Число не инициализировано.";
 		BigInt c = *this - value;
 		int64_t *bI_buffer = c.getBigInt();
 		return !bI_buffer[1];
@@ -146,7 +171,7 @@ public:
 		BigInt c = value;
 		return !(*this > c);
 	}
-	int64_t bigIntCMP(int64_t* la, int64_t*  lb) const { //Метод класса BigInt, СРАВНЕНИЕ КОЛ-ВА РАЗРЯДОВ у двух длинных чисел
+	int64_t bigIntCMP(int64_t* la, int64_t*  lb) const {
 		if (abs(la[0]) > abs(lb[0]))return 1;
 		if (abs(la[0]) < abs(lb[0]))return -1;
 		for (int64_t i = abs(la[0]); i >= 1; i--) {
@@ -155,14 +180,14 @@ public:
 		}
 		return 0;
 	}
-	int64_t max(int64_t a, int64_t b) const {//Метод класса BigInt,          max
+	int64_t max(int64_t a, int64_t b) const {
 		int64_t c = (a > b) ? a : b;
 		return c;
 	}
 	int64_t* getBigInt() const {
 		return bI;
 	}
-	void strTolong(string a, int64_t* la) { //Метод класса BigInt, преобразует СТРОКУ в ДЛИННОЕ ЧИСЛО (int*)
+	void strTolong(string a, int64_t* la) {
 		if (a[0] == '-') {
 			la[0] = -static_cast<int64_t>(ceil((double)(a.size() - 1) / BASELEN));
 			a.erase(0, 1);
@@ -192,11 +217,11 @@ public:
 			a /= 10;
 		}
 	}
-	int64_t sgn(int64_t a) const { //Метод класса BigInt,                sgn
+	int64_t sgn(int64_t a) const {
 		if (!a)return 0;
 		return a / abs(a);
 	}
-	int digitCount(int64_t a) const { //Метод класса BigInt, выдаёт КОЛ-ВО РАЗРЯДОВ в числе (в 10-ричной системе)
+	int digitCount(int64_t a) const {
 		int64_t count = 0;
 		if (!a)return 1;
 		while (a) {
@@ -205,7 +230,7 @@ public:
 		}
 		return count;
 	}
-	int64_t atois(string str) {//Метод класса BigInt, ПРЕОБРАЗОВАНИЕ STRING В ЧИСЛО
+	int64_t atois(string str) {
 		const char *c = str.c_str();
 		int64_t a = atoi(c);
 		return a;
@@ -216,7 +241,6 @@ private:
 ostream& operator<<(ostream& out, const BigInt& value)
 {
 	int64_t *bI = value.getBigInt();
-	if (!bI[0]) throw "Ошибка вывода. Число не инициализировано.";
 	out << value.sgn(bI[0]) * bI[abs(bI[0])];
 	int64_t dc;
 	for (int64_t i = abs(bI[0]) - 1; i >= 1; i--) {
